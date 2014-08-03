2cbc1c39e1b1583322cb598370683783f34c179d
Paul Wenig <pwenig@gmail.com>
2014-08-03 14:26:41 -0600

    User cannot select a playdate in the past

--- app/models/playdate.rb
+++ app/models/playdate.rb
@@ -1,4 +1,13 @@
 class Playdate < ActiveRecord::Base
   has_many :puppies
   belongs_to :puppy
+
+  validate :date_in_past
+
+  def date_in_past
+    if date.past? == true
+      errors.add(:date, "You cannot choose a date in the past!")
+    end
+  end
+
 end
