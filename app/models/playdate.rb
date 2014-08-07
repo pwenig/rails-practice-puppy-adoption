e0e34c496a174dee5e5d930dab8cdc72b31b1bb7
Paul Wenig <pwenig@gmail.com>
2014-08-06 18:49:16 -0600

    Added model spec for validations

--- app/models/playdate.rb
+++ app/models/playdate.rb
@@ -1,5 +1,13 @@
 class Playdate < ActiveRecord::Base
-belongs_to :puppy
-has_many :puppies
+  belongs_to :puppy
+  has_many :puppies
+
+  validate :date_in_past
+
+  def date_in_past
+    if date.past? == true
+      errors.add(:date, "You cannot have a date in the past")
+    end
+  end
 
 end
--- spec/models/playdate_spec.rb
+++ spec/models/playdate_spec.rb
@@ -8,4 +8,11 @@ describe 'Playdate' do
     playdate = Playdate.new(puppy_id: puppy.id, date: "2014-09-01", attending: 2)
     expect(playdate.valid?).to eq true
   end
+
+  it "does not create a playdate with a date in the past" do
+    puppy =  Puppy.new(image: "some string")
+    playdate = Playdate.new(puppy_id: puppy.id, date: "2014-01-01", attending: 2)
+    expect(playdate.valid?).to eq false
+
+  end
 end
